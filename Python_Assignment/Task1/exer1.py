123f8a69091ec29c5375cd5571b211b6b7c4dbf2
Nachamai <nachviji84@gmail.com>
2021-04-07 15:37:51 -0500

    adding first three python code

---
+++ Python_Assignment/Task1/exer1.py
@@ -0,0 +1,2 @@
+a = 1; b = 'apple'; c = 12.5
+print(a,b,c)
---
+++ Python_Assignment/Task1/exer3.py
@@ -0,0 +1,15 @@
+a = 3 + 2j
+b = 2
+print(a,b)
+c = b
+b = a
+a = c
+print(a,b)
+
+c = 2
+d = 4
+print(c,d)
+c = c + d
+d = c - d
+c = c - d
+print(c,d)
