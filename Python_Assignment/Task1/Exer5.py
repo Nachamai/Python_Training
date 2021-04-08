72cb2f533214bd81a267d4f806645de57bd27d35
Nachamai <nachviji84@gmail.com>
2021-04-07 21:07:07 -0500

    added files for Exer5 and Exer 6

---
+++ Python_Assignment/Task1/Exer5.py
@@ -0,0 +1,5 @@
+a = int(input("Enter the first number between 1 to 10:"))
+b = int(input("Enter the second number between 1 to 10:"))
+z = a + b
+result = 30 + z
+print(result)
---
+++ Python_Assignment/Task1/Exer6.py
@@ -0,0 +1,2 @@
+x = eval(input("enter the value:"))
+print(type(x))
