7d8a8f9556c11be9a93a21d42823347c33adf260
Nachamai <nachviji84@gmail.com>
2021-04-07 21:53:36 -0500

    added files for Exer7 and Exer8

---
+++ Python_Assignment/Task1/Exer7.py
@@ -0,0 +1,8 @@
+uppercase = "HELLO"
+lowercamel = "hiHowRU"
+uppercamel = "HiHowRU"
+snakecase = "hi_how_r_u"
+print(uppercase)
+print(lowercamel)
+print(uppercamel)
+print(snakecase)
---
+++ Python_Assignment/Task1/Exer8.py
@@ -0,0 +1,7 @@
+x = 5
+print(id(x))
+x = 'str'
+print(id(x))
+print(x)
+
+'''The variable x is just a label, first it was holding a value called 5 in a memory location, once it got re-assigned to a new value, it starts to hold a new string value in a different physical memory location'''
